f13e2b015e3a0a1b2962e0d02b0ee43743118b0d
Vedangi <vedangidhole10@gmail.com>
2022-07-31 18:58:00 +0530

    Quiz-App

---
+++ app/src/main/java/com/adpth/quizapp1/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.adpth.quizapp1;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is capital of India?",
+            "How many seconds are there in a minute?",
+            "Rainbow consists of how many colours?",
+            "Name the National animal of India"
+    };
+
+    public static String choices[][] = {
+            {"kolkata","Maharashtra","Delhi","Noida"},
+            {"60","90","80","100"},
+            {"3","8","7","5"},
+            {"Cat","Lion","Dog","Tiger"}
+    };
+
+    public static String correctAnswers[] = {
+            "Delhi",
+            "60",
+            "7",
+            "Tiger"
+    };
+
+}
